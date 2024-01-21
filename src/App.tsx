4cabd7d2d6b65769538907e3b9c1bd6f48e771ea
jkr <jkr13850@gmail.com>
2024-01-21 18:13:43 -0500

    GH pages.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,14 @@ enum AppTexts {
 const App: FC = () => {
   return (
     <div>
-      <div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
         <h1>{AppTexts.NAME}</h1>
         <NavControls />
       </div>
