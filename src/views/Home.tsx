7daebafe36f17214698a1a5bc858a95f1a6cebee
jkr <jkr13850@gmail.com>
2024-01-21 19:17:24 -0500

    Adding some dummy text.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
+import { loremIpsumOne, loremIpsumTwo } from '../utils';
 
 export const Home: FC = () => {
   return (
     <div>
       <h1>Home</h1>
+      <p>{loremIpsumOne}</p>
+      <p>{loremIpsumTwo}</p>
     </div>
   );
 };
